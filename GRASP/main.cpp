4907124fbd7410f7a2166bfffba5d3fb83f3a0aa
Jiří Procházka <ojirio@gmail.com>
2011-04-03 19:34:35 +0200

    cleanup

--- GRASP/main.cpp
+++ GRASP/main.cpp
@@ -30,29 +30,7 @@ int main(int argc, char *argv[])
         return(1);
     }
 
- /*   librdf_node* s = librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/");
-    if(!s) fprintf(stderr, "Failed to make example node\n");
-    librdf_node* p = librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/");
-    librdf_node* o = librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/");
-    librdf_node* g = librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/");
-    librdf_statement *t = librdf_new_statement_from_nodes(rdf::world, s, p, o);
-    if(!t) fprintf(stderr, "Failed to make example triple\n");
-    //if(!librdf_model_context_add_statement(rdf::model, g, t)) fprintf(stderr, "Failed to add test triple\n");
-    //if(!librdf_model_add(rdf::model, s, p, o)) fprintf(stderr, "Failed to add test triple\n");
-    if(0 != librdf_model_context_add_statement(rdf::model, librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/"), 
-               librdf_new_statement_from_nodes(rdf::world, 
-                   librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/"), 
-                   librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/"), 
-                   librdf_new_node_from_uri_string(rdf::world, (unsigned char const *)"http://example.com/")))) fprintf(stderr, "Failed to add test triple\n");
-*/
-    //PersistentCounter pc;
-    //pc.init(QString(PERSCOUNTERPATH));
-    //return pc.increment();
-    //printf("%li\n", PersistentCounter::increment(PERSCOUNTERPATH));
 
-    librdf_node *cx = rdf::loadGraphFromURI(QString("http://mud.cz/foaf.rdf")); //, "application/rdf+xml");
-    rdf::printContext(cx);
-    librdf_free_node(cx);
 
 
     QApplication a(argc, argv);
