a626820045845a4a1949e69cbd0b01f3a644f302
Jiří Procházka <ojirio@gmail.com>
2012-03-10 07:17:17 +0100

    fix edge label center position

--- GRASP/graphedge.cpp
+++ GRASP/graphedge.cpp
@@ -68,14 +68,27 @@ void GraphEdge::updateGeometry()
     // line from center of sourceNode_ to center of destNode_
     QRectF sSBR (sourceNode_->sceneBoundingRect());
     QRectF dSBR (destNode_->sceneBoundingRect());
-    QPointF sP (sSBR.center());
-    QPointF dP (dSBR.center());
-    QLineF line(sP, dP);
+    QLineF centerline(sSBR.center(), dSBR.center());
 
+    // set the source and destination points
+    if(!sSBR.intersects(dSBR)) {
+        QPointF po;
+        if(QLineF::BoundedIntersection == centerline.intersect(QLineF(sSBR.topLeft(), sSBR.topRight()), &po)) sourcePoint = po;
+        else if(QLineF::BoundedIntersection == centerline.intersect(QLineF(sSBR.bottomRight(), sSBR.topRight()), &po)) sourcePoint = po;
+        else if(QLineF::BoundedIntersection == centerline.intersect(QLineF(sSBR.bottomLeft(), sSBR.bottomRight()), &po)) sourcePoint = po;
+        else if(QLineF::BoundedIntersection == centerline.intersect(QLineF(sSBR.topLeft(), sSBR.bottomLeft()), &po)) sourcePoint = po;
+        if(QLineF::BoundedIntersection == centerline.intersect(QLineF(dSBR.topLeft(), dSBR.topRight()), &po)) destPoint = po;
+        else if(QLineF::BoundedIntersection == centerline.intersect(QLineF(dSBR.bottomRight(), dSBR.topRight()), &po)) destPoint = po;
+        else if(QLineF::BoundedIntersection == centerline.intersect(QLineF(dSBR.bottomLeft(), dSBR.bottomRight()), &po)) destPoint = po;
+        else if(QLineF::BoundedIntersection == centerline.intersect(QLineF(dSBR.topLeft(), dSBR.bottomLeft()), &po)) destPoint = po;
+    } else {
+        sourcePoint = destPoint = (sSBR.bottomLeft().y() < dSBR.bottomLeft().y()) ? sSBR.bottomLeft() : dSBR.bottomLeft();
+    }
+    QLineF line(sourcePoint, destPoint);
     // set label centered in the middle of the arrow
     QRectF label (label_->boundingRect());
     label.translate(line.pointAt(0.5));
-    label.translate(-label.width()/2, -label.height()/2);
+    if(sourcePoint != destPoint) label.translate(-label.width()/2, -label.height()/2);
 
     // avoid overlapping
     bool overlapped;
@@ -111,20 +124,7 @@ void GraphEdge::updateGeometry()
 
     prepareGeometryChange();
 
-    // set the source and destination points
-    if(!sSBR.intersects(dSBR)) {
-        QPointF po;
-        if(QLineF::BoundedIntersection == line.intersect(QLineF(sSBR.topLeft(), sSBR.topRight()), &po)) sourcePoint = po;
-        else if(QLineF::BoundedIntersection == line.intersect(QLineF(sSBR.bottomRight(), sSBR.topRight()), &po)) sourcePoint = po;
-        else if(QLineF::BoundedIntersection == line.intersect(QLineF(sSBR.bottomLeft(), sSBR.bottomRight()), &po)) sourcePoint = po;
-        else if(QLineF::BoundedIntersection == line.intersect(QLineF(sSBR.topLeft(), sSBR.bottomLeft()), &po)) sourcePoint = po;
-        if(QLineF::BoundedIntersection == line.intersect(QLineF(dSBR.topLeft(), dSBR.topRight()), &po)) destPoint = po;
-        else if(QLineF::BoundedIntersection == line.intersect(QLineF(dSBR.bottomRight(), dSBR.topRight()), &po)) destPoint = po;
-        else if(QLineF::BoundedIntersection == line.intersect(QLineF(dSBR.bottomLeft(), dSBR.bottomRight()), &po)) destPoint = po;
-        else if(QLineF::BoundedIntersection == line.intersect(QLineF(dSBR.topLeft(), dSBR.bottomLeft()), &po)) destPoint = po;
-    } else {
-        sourcePoint = destPoint = line.p1();
-    }
+
     QGraphicsLayoutItem::updateGeometry();
 }
 
